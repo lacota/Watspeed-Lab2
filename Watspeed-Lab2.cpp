abbcb5b530eb99b4c0f77d1c75d4c32aa6bda74f
Robert Schmidt <roberts_lacota@hotmail.com>
2024-06-19 09:23:55 -0400

    Add project files.

---
+++ Watspeed-Lab2.cpp
@@ -0,0 +1,12 @@
+﻿// Watspeed-Lab2.cpp : Defines the entry point for the application.
+//
+
+#include "Watspeed-Lab2.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
